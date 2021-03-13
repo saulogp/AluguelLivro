7f41e43bc13c58d0b51ce00080669f91c6c80d25
saulogp <sauloproetti@gmail.com>
2021-03-13 14:59:33 -0300

    create class Cliente and Endereco

---
+++ AluguelLivro/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelLivro
+{
+    class Cliente
+    {
+        public long IdCliente { get; set; }
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public Endereco Endereco { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IdCliente},{CPF},{Nome},{DataNascimento.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)},{Telefone},{Endereco.Logradouro},{Endereco.Bairro},{Endereco.Cidade},{Endereco.Estado},{Endereco.CEP}";
+        }
+    }
+}
---
+++ AluguelLivro/Endereco.cs
@@ -0,0 +1,11 @@
+﻿namespace AluguelLivro
+{
+    class Endereco
+    {
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string CEP { get; set; }
+    }
+}
