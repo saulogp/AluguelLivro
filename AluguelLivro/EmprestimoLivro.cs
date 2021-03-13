4c644e49d45f2d2bca08efe05f9b600ddcc97d36
saulogp <sauloproetti@gmail.com>
2021-03-13 15:32:29 -0300

    create class EmprestimoLivro

---
+++ AluguelLivro/EmprestimoLivro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelLivro
+{
+    class EmprestimoLivro
+    {
+        public long IdCliente { get; set; }
+        public long NumeroTombo { get; set; }
+
+        public DateTime DataEmprestimo { get; set; }
+        public DateTime DataDevolucao { get; set; }
+        public int StatusEmprestimo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IdCliente},{NumeroTombo},{DataEmprestimo.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)},{DataDevolucao.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)},{StatusEmprestimo}";
+        }
+    }
+}
